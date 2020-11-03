94fef393cc454d165d67aabcaec1511d4eac26e0
massoudasadi <massoud.asadi@hotmail.com>
2020-11-03 19:40:33 +0330

    arp header added & ip & tcp edited & readme update

--- README.md
+++ README.md
@@ -1,4 +1,37 @@
 <img src="./packiffer.png">
 
-# packiffer
-cross-platform packet sniffer
+# Packiffer
+Packiffer is a lightweight single file cross-platform packet sniffer that let you sniff packets live from network interface or offline pcap files. you can also apply filters and promiscuous mode on interface.
+
+# Prerequisites For Binary
+Libpcap v1.9.1
+
+# Prerequisites For Source
+Golang v1.15.2
+GoPacket v1.1.18
+Libpcap v1.9.1
+
+# How to get Packiffer
+Checkout packiffer git repo using git clone
+```
+git clone https://github.com/massoudasadi/packiffer.git
+cd packiffer
+go run packiffer.go -i eth0
+```
+
+# Run Packiffer
+Display list of network interfaces
+```
+./packiffer -d
+```
+
+Sniff Packets on 'eth0' and save packets in 'eth0.pcap' until Ctrl+C pressed 
+```
+./packiffer -i eth0
+```
+
+Sniff Packets on 'eth0' and save packets in 'eth0.pcap' (promiscuous mode) until Ctrl+C pressed
+```
+./packiffer -i eth0 -p
+```
+
--- packiffer.go
+++ packiffer.go
@@ -146,31 +146,43 @@ func (p *packiffer) packetInfo(packet *gopacket.Packet) {
 		fmt.Println()
 	}
 
-	ipLayer := (*packet).Layer(layers.LayerTypeIPv4)
-	if ipLayer != nil {
-		fmt.Println("IPv4 layer detected.")
-		ip, _ := ipLayer.(*layers.IPv4)
-
-		fmt.Printf("From %s to %s\n", ip.SrcIP, ip.DstIP)
-		fmt.Println("Protocol: ", ip.Protocol)
+	arpLayer := (*packet).Layer(layers.LayerTypeARP)
+	if arpLayer != nil {
+		fmt.Println("Arp layer detected.")
+		arp, _ := arpLayer.(*layers.ARP)
+		fmt.Printf("AddrType: %d", arp.AddrType)
+		fmt.Printf("Protocol: %d", arp.Protocol)
+		fmt.Printf("SourceHwAddress: %d", arp.SourceHwAddress)
+		fmt.Printf("SourceProtAddress: %d", arp.SourceProtAddress)
+		fmt.Printf("DstHwAddress: %d", arp.DstHwAddress)
+		fmt.Printf("DstProtAddress: %d", arp.DstProtAddress)
 		fmt.Println()
+	}
+
+	ipv4Layer := (*packet).Layer(layers.LayerTypeIPv4)
+	if ipv4Layer != nil {
+		fmt.Println("IPv4 layer detected.")
+		ipv4, _ := ipv4Layer.(*layers.IPv4)
+		fmt.Printf("From %s to %s", ipv4.SrcIP, ipv4.DstIP)
+		fmt.Println("Protocol: ", ipv4.Protocol)
+		fmt.Println("TTL: ", ipv4.TTL)
+		fmt.Println("TOS: ", ipv4.TOS)
+		fmt.Println("IHL: ", ipv4.IHL)
 	}
 
 	tcpLayer := (*packet).Layer(layers.LayerTypeTCP)
 	if tcpLayer != nil {
 		fmt.Println("TCP layer detected.")
 		tcp, _ := tcpLayer.(*layers.TCP)
-
-		fmt.Printf("From port %d to %d\n", tcp.SrcPort, tcp.DstPort)
+		fmt.Printf("From port %d to %d", tcp.SrcPort, tcp.DstPort)
 		fmt.Println("Sequence number: ", tcp.Seq)
-		fmt.Println()
+		fmt.Println("ACK: ", tcp.Ack)
 	}
 
 	udpLayer := (*packet).Layer(layers.LayerTypeUDP)
 	if udpLayer != nil {
 		fmt.Println("UDP layer detected.")
 		udp, _ := udpLayer.(*layers.UDP)
-
 		fmt.Printf("From port %d to %d\n", udp.SrcPort, udp.DstPort)
 		fmt.Println("Checksum number: ", udp.Checksum)
 		fmt.Println()
@@ -199,23 +211,13 @@ func (p *packiffer) packetInfo(packet *gopacket.Packet) {
 	}
 }
 
-func (p *packiffer) dumpPacket(packets *gopacket.Packet) {
-	var f *os.File
-	if outputFlag == true {
-		f, _ = os.Create(p.output + ".pcap")
-	} else {
-		f, _ = os.Create(p.interfaceName + ".pcap")
-	}
-	w := pcapgo.NewWriter(f)
-	w.WriteFileHeader(snapshotLen, layers.LinkTypeEthernet)
+func (p *packiffer) dumpPacket(packets *gopacket.Packet, w *pcapgo.Writer) {
 	w.WritePacket((*packets).Metadata().CaptureInfo, (*packets).Data())
 	packetCount++
-
 	if limitFlag == true && (packetCount > int64(packetLimit)) {
 		fmt.Printf("\n%d packets captured on %s", packetLimit, p.interfaceName)
 		os.Exit(0)
 	}
-	defer f.Close()
 }
 
 func (p *packiffer) injectPacket() {
@@ -255,9 +257,19 @@ func (p *packiffer) openLivePcap() {
 	if filterFlag == true {
 		p.filterPacket()
 	}
+	packetCount = 0
+	var f *os.File
+	if outputFlag == true {
+		f, _ = os.Create(p.output + ".pcap")
+	} else {
+		f, _ = os.Create(p.interfaceName + ".pcap")
+	}
+	w := pcapgo.NewWriter(f)
+	w.WriteFileHeader(snapshotLen, layers.LinkTypeEthernet)
+	defer f.Close()
 	packets := gopacket.NewPacketSource(p.handle, p.handle.LinkType())
 	for packet := range packets.Packets() {
-		go p.dumpPacket(&packet)
+		go p.dumpPacket(&packet, w)
 	}
 }
 
